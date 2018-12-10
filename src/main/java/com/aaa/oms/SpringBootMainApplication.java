59846d624dd73a56b5f7a48ff8697535b64cf4bd
dztwwj <349921311@qq.com>
2018-12-10 14:44:17 +0800

    Initial commit

---
+++ src/main/java/com/aaa/oms/SpringBootMainApplication.java
@@ -0,0 +1,18 @@
+package com.aaa.oms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * className:SpringBootMainApplication
+ * discription:
+ * author:LiuQian
+ * createTime:2018-12-10 14:43:07
+ */
+@SpringBootApplication
+public class SpringBootMainApplication {
+
+    public static void main(String[] args){
+        SpringApplication.run(SpringBootMainApplication.class);
+    }
+}
